8cbb8453c135897fe230a10f37be9e4e57e149a4
thong0128 <thong0128@gmail.com>
2023-11-08 14:20:51 +0700

    first update

---
+++ Practice-1.js
@@ -0,0 +1,6 @@
+function showMessage(){
+    alert('Xin chào!');
+    document.getElementById('messageDiv').innerHTML = 'Xin chào!';
+    document.write('Xin chào!');
+    console.log('Xin chào!');
+}
